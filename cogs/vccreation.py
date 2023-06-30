e2a9ac372cd98d0154f40bbce1a284946d86f7c6
NEO EVIL <femtostream@gmail.com>
2023-06-29 23:02:27 -0700

    Fixed Positioning!

--- cogs/vccreation.py
+++ cogs/vccreation.py
@@ -21,8 +21,8 @@ class VcCreation(Cog):
 		if vc_after := after.channel:
 			if vc_after.id == self.CREATOR_CHANNEL_ID:
 				voice_channel = await member.guild.create_voice_channel(
-					name=f"{member.nick}'s VC",
-				)
+					name=f"{member.display_name}'s VC", category=vc_after.category
+				) 
 				await member.move_to(voice_channel)
 				self.temp_channels.add(voice_channel.id)
 			await voice_channel.set_permissions(member, administrator=True)
@@ -34,6 +34,12 @@ class VcCreation(Cog):
 		print("Server Voice Channels: ", ctx.author.guild.voice_channels)
 		print("Server Text Channels: ", ctx.author.guild.text_channels)
 		print(ctx.channel.permissions_for(ctx.author).administrator)
+		print(ctx.channel.category_id)
+		print(ctx.guild.categories)
+		print(ctx.author.nick) #Useless
+		print(ctx.author.name) #Discord Code Name
+		print(ctx.author.display_name) #Server Nickname
+		print(ctx.author.global_name) #Actual Display Name
 
  
 				
