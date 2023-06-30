18803ab47b9201cd709ca4b1e4fd1da05f1561c9
NEO EVIL <femtostream@gmail.com>
2023-06-29 23:32:46 -0700

    Implement Single VC per Voice-Owner Rule

--- cogs/vccreation.py
+++ cogs/vccreation.py
@@ -9,23 +9,34 @@ class VcCreation(Cog):
 	def __init__(self, bot: Bot):
 		self.bot = bot
 		self.CREATOR_CHANNEL_ID = int(os.getenv('CREATOR_CHANNEL_ID'))
-		self.temp_channels = set()
+		self.temp_channels = {}
 
 	@Cog.listener()
 	async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
-		if vc_before := before.channel:
-			if vc_before.id in self.temp_channels and not vc_before.members:
-				await vc_before.delete()
-				self.temp_channels.remove(vc_before)
-				
+		'''
+		The bot should be able to create VC's in the voice channel category.
+		If you are the admin of a VC, it will not let you make a new one.
+		However, this breaks if you do it fast enough.. TODO FIX THIS.
+		VcCreation uses a temp channels dictionary to do this.
+		'''
+
 		if vc_after := after.channel:
 			if vc_after.id == self.CREATOR_CHANNEL_ID:
-				voice_channel = await member.guild.create_voice_channel(
-					name=f"{member.display_name}'s VC", category=vc_after.category
-				) 
-				await member.move_to(voice_channel)
-				self.temp_channels.add(voice_channel.id)
-			await voice_channel.set_permissions(member, administrator=True)
+				if member.id not in self.temp_channels: #check if you already own a channel
+					voice_channel = await member.guild.create_voice_channel(
+						name=f"{member.display_name}'s VC", category=vc_after.category
+					) 
+					#await member.move_to(voice_channel)
+					self.temp_channels[member.id] = voice_channel.id
+					await voice_channel.set_permissions(member, administrator=True)
+				await member.move_to(member.guild.get_channel(self.temp_channels[member.id]))
+
+		if vc_before := before.channel:
+			if vc_before.id in self.temp_channels.values() and not vc_before.members:
+				keys = [k for k, v in self.temp_channels.items() if v == vc_before.id]
+				await vc_before.delete()
+				self.temp_channels.pop(keys)
+
 
 	#Created a testing command for the purposes of debugging
 	@command(aliases=['t'])
